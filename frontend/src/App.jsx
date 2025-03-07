9b49d3a2c6a5d971ca0f0ba2a46a15fb0274c834
Suprim Khatri <supremekhatri62@gmail.com>
2025-03-07 20:27:51 +0545

    project setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import './index.css';
+
+function App() {
+  return (
+    <div className="bg-indigo-700 text-lg">
+          <h1 className="">Welcome to NoteNest</h1>
+          <p className="">Tailwind CSS is working!</p>
+      </div>
+  );
+}
+
+export default App;
