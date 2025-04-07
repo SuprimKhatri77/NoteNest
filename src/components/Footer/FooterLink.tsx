e55ca90ffaaece4c52a37b4dd0b45cd48057701d
SuprimKhatri77 <supremekhatri62@gmail.com>
2025-04-07 20:35:37 +0545

    resolved deployment issues

--- src/components/Footer/FooterLink.tsx
+++ src/components/Footer/FooterLink.tsx
@@ -1,6 +1,10 @@
 import React from 'react'
 
-function FooterLink({text}) {
+interface FooterLink  {
+  text: string;
+}
+
+function FooterLink({text}: FooterLink) {
   return (
     <li className="mb-3">
     <a href="#" className="text-gray-300 text-base transition-all hover:text-white hover:translate-x-1 inline-block">{text}</a>
